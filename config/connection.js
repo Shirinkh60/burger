97089b5a8ae6cbc4312bd3b218c8ae3b5eaa3542
Shirin <shirin.boroujeni@gmail.com>
2018-09-30 11:35:54 -0700

    connection.js changed

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,6 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+var connection; 
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -18,12 +13,6 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
